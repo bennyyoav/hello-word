3775d09082a35eb6f033045260fda0584fa86681
benny <bennyyoav@gmail.com>
2020-01-19 00:51:37 +0200

    add fibielemReq func

--- hello/demo.py
+++ hello/demo.py
@@ -1,8 +1,25 @@
 "add  new discription"
 def twice(x):
     return 2*x
-def text():
+def printHello():
      return ("hello")
+
+def fibielemReq(num):
+    if num==1:
+        return 0
+    elif num==2:
+        return 1
+    else:
+        return fibielemReq(num - 1) + fibielemReq(num - 2)
+
+
 if __name__=="__main__":
-    print (text())
-    print (utiles.twice(10))
+    print (fibielemReq(1))
+    print (fibielemReq(2))
+    print (fibielemReq(3))
+
+
+
+
+
+

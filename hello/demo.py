09e6216cb9bd2a2c5d880306caadba6045ce8f33
benny <bennyyoav@gmail.com>
2020-01-19 01:14:23 +0200

    add fibieSerial func

--- hello/demo.py
+++ hello/demo.py
@@ -1,25 +1,30 @@
 "add  new discription"
+
+
 def twice(x):
-    return 2*x
+    return 2 * x
+
+
 def printHello():
-     return ("hello")
+    return ("hello")
+
 
 def fibielemReq(num):
-    if num==1:
+    if num == 0:
         return 0
-    elif num==2:
+    elif num == 1:
         return 1
     else:
         return fibielemReq(num - 1) + fibielemReq(num - 2)
 
 
-if __name__=="__main__":
-    print (fibielemReq(1))
-    print (fibielemReq(2))
-    print (fibielemReq(3))
+def fibieSerial(size):
+    fibiSerial = []
+    for elem in range(size):
+        fibiSerial.append(fibielemReq(elem))
+        print (elem)
+    return fibiSerial
 
 
-
-
-
-
+if __name__ == "__main__":
+    print(fibieSerial(10))
